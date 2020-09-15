1b3fbbceb1471c3700271a4a7f250adb692625da
Takao21 <42912031+Takao21@users.noreply.github.com>
2020-09-15 16:28:40 +0630

    code cleaning

--- src/components/TrelloList.js
+++ src/components/TrelloList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import TrelloCard from "./TrelloCard";
 import ListMenu from "./ListMenu";
 import "./TrelloList.css";
