0cf4ea1fdc5d68b95e336eccc1bfcd20a50247e4
Takao21 <42912031+Takao21@users.noreply.github.com>
2020-09-17 19:10:46 +0630

    rough layout for card details done

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -4,8 +4,36 @@ import "./CardDetails.css";
 
 function CardDetails() {
   return ReactDOM.createPortal(
-    <div className="CardDetails bg-light">
-      <h1>Welcome to modal.</h1>
+    <div className="CardDetails bg-light container-fluid">
+      <div className="row details-head">
+        <div className="col-12">
+          <h3>Header</h3>
+          <p>Web Resource Reference</p>
+        </div>
+      </div>
+      <div className="row details-body">
+        <div className="col-9">content</div>
+        <div className="col-3 d-flex flex-column">
+          <div className="btn btn-primary my-1">button</div>
+          <div className="btn btn-primary my-1">button</div>
+          <div className="btn btn-primary my-1">button</div>
+          <div className="btn btn-primary my-1">button</div>
+          <div className="btn btn-primary my-1">button</div>
+          <div className="btn btn-primary my-1">button</div>
+          <div className="btn btn-primary my-1">button</div>
+          <div className="btn btn-primary my-1">button</div>
+          <div className="btn btn-primary my-1">button</div>
+          <div className="btn btn-primary my-1">button</div>
+          <div className="btn btn-primary my-1">button</div>
+          <div className="btn btn-primary my-1">button</div>
+          <div className="btn btn-primary my-1">button</div>
+          <div className="btn btn-primary my-1">button</div>
+          <div className="btn btn-primary my-1">button</div>
+          <div className="btn btn-primary my-1">button</div>
+          <div className="btn btn-primary my-1">button</div>
+          <div className="btn btn-primary my-1">button</div>
+        </div>
+      </div>
     </div>,
     document.getElementById("modal-root")
   );
